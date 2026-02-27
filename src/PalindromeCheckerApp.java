3f0b37b198373b998e52c14132663e42cc842f05
S-anskriti <sanscode19@gmail.com>
2026-02-28 03:14:18 +0530

    UC6- Queue and stack implementation

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,25 +1,30 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-
-        String Palindrome = "racecar", Reversed = "";
+        String Palindrome = "racecar", ReversedQ = "", ReversedS = "";
         char[] chars = Palindrome.toCharArray();
         Stack <Character> stack  = new Stack<>();
+        Queue <Character> queue = new LinkedList<>();
+
         int i = 0, n = chars.length;
 
-        for(i = 0; i < n; i++){
-            stack.push(chars[i]);
+        for(char ch : chars){
+            stack.push(ch);
+            queue.add(ch);
         }
 
-        while(!stack.empty()){
-            Reversed += stack.pop();
+        while (!queue.isEmpty() && !stack.empty()) {
+            ReversedQ += queue.remove();
+            ReversedS += stack.pop();
         }
 
-        if (Palindrome.equals(Reversed)) {
-            System.out.println("The string " + Palindrome + " is palindrome");
-        } else {
-            System.out.println("The string " + Palindrome + " is not a palindrome");
-        }
-
+        if (Palindrome.equals(ReversedS))
+            System.out.println("The Stack method worked.");
+        if (Palindrome.equals(ReversedQ))
+            System.out.println("The Queue method worked");
+        else
+            System.out.println("None of the methods worked");
     }
 }
