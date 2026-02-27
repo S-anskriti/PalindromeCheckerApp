03ce6ec5a1a4f33006c5b2d168b51a65a8e6800e
S-anskriti <sanscode19@gmail.com>
2026-02-28 03:05:09 +0530

    UC4 - CharArray implementation

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,14 +1,22 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
+
         String Palindrome = "racecar";
-        String Reverse = "";
+        char[] chars = Palindrome.toCharArray();
 
-        int i;
+        boolean flag = true;
+        int start = 0, end = Palindrome.length()-1;
 
-        for(i = Palindrome.length()-1; i >= 0; i--){
-            Reverse += Palindrome.charAt(i);
+        while (start < end){
+            if(chars[start] != chars[end]){
+                flag = false;
+                break;
+            }
+            start ++;
+            end --;
         }
-        if (Palindrome.equals(Reverse))  System.out.println("The string " + Palindrome + " is a palindrome.");
+
+        if (flag)  System.out.println("The string " + Palindrome + " is a palindrome.");
         else System.out.println("The string " + Palindrome + " is not a palindrome.");
     }
 }
