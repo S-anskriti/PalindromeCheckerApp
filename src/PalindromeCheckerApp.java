85ff9dfd80f0e67b834458873cec2479fdaaefb0
S-anskriti <sanscode19@gmail.com>
2026-02-28 14:28:29 +0530

    UC9 Recurssive Palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,71 +1,28 @@
 public class PalindromeCheckerApp {
-    static class Node {
-        int data;
-        Node next;
 
-        Node(int data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-    static Node head = null;
-    public static void insert(int data) {
-        Node newNode = new Node(data);
+    public static boolean isPalindrome(String word, int start, int end) {
 
-        if (head == null) {
-            head = newNode;
-            return;
-        }
-
-        Node temp = head;
-        while (temp.next != null) {
-            temp = temp.next;
-        }
-        temp.next = newNode;
-    }
-    public static boolean isPalindrome() {
-
-        if (head == null || head.next == null) {
+        if (start >= end) {
             return true;
         }
 
-        Node slow = head;
-        Node fast = head;
-        Node prev = null;
-        Node next;
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-
-            next = slow.next;
-            slow.next = prev;
-            prev = slow;
-            slow = next;
-        }
-        if (fast != null) {
-            slow = slow.next;
-        }
-        while (prev != null && slow != null) {
-            if (prev.data != slow.data) {
-                return false;
-            }
-            prev = prev.next;
-            slow = slow.next;
+        if (word.charAt(start) != word.charAt(end)) {
+            return false;
         }
 
-        return true;
+        return isPalindrome(word, start + 1, end - 1);
     }
 
     public static void main(String[] args) {
-        insert(1);
-        insert(2);
-        insert(3);
-        insert(2);
-        insert(1);
 
-        if (isPalindrome()) {
-            System.out.println("Linked List is a palindrome");
+        String word = "madam";
+
+        boolean result = isPalindrome(word, 0, word.length() - 1);
+
+        if (result) {
+            System.out.println(word + " is a palindrome");
         } else {
-            System.out.println("Linked List is not a palindrome");
+            System.out.println(word + " is not a palindrome");
         }
     }
 }
